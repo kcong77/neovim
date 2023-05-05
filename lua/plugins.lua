c62562dbb92672c94dd6ec4364dd706375d96f42
Kay Nguyen <kcong.nguyen@icloud.com>
2023-05-04 23:37:07 -0500

    config plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,1 +1,16 @@
-print("hello world")
+local packer_ok, packer = pcall(require, "packer")
+if not packer_ok then
+  return
+end
+
+packer.init({
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+end)
