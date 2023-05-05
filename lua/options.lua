acc38aa1a2a4843c6ba73d6626169c4f30d63565
Kay Nguyen <kcong.nguyen@icloud.com>
2023-05-05 00:09:36 -0500

    add options

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.wrap = false
+vim.opt.clipboard = "unnamedplus"
