439dc0e3aa136d249f7700a73ddfdaa03b90febe
Kay Nguyen <kcong.nguyen@icloud.com>
2023-05-05 01:09:45 -0500

    bootstrap packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local packer_ok, packer = pcall(require, "packer")
 if not packer_ok then
   return
@@ -19,5 +32,7 @@ return packer.startup(function(use)
   use  {"nvim-lualine/lualine.nvim", 
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
-
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
