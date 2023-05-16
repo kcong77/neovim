7038aa55c2ec6cb2baf4c1daefc4746d6a45596c
Kay N <kcong.nguyen@icloud.com>
2023-05-16 00:24:18 -0500

    change leader key

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,10 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
@@ -9,6 +13,9 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<leader>s", "<CMD>lua vim.lsp.buf.format()<CR><CMD>w<CR>", opts)
 keymap("n", "<leader>q", "<CMD>wq<CR>", opts)
 keymap("n", "<leader>f", "<CMD>Telescope find_files<CR>", opts)
+keymap("n", "<leader>g", "<CMD>Telescope grep_string<CR>", opts)
+keymap("n", "<leader>h", "<CMD>Telescope live_grep<CR>", opts)
+keymap("n", "<leader>h", "<CMD>Telescope live_grep<CR>", opts)
 keymap("n", "<leader>g", "<CMD>Telescope grep_string<CR>", opts)
 keymap("n", "<leader>h", "<CMD>Telescope live_grep<CR>", opts)
 keymap("n", "<leader>h", "<CMD>Telescope live_grep<CR>", opts)
