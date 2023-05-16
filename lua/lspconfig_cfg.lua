0aaf8ef78d4befa005fd4e0948d87d0042cc6944
Kay N <kcong.nguyen@icloud.com>
2023-05-15 21:53:26 -0500

    final ****************

--- lua/lspconfig_cfg.lua
+++ lua/lspconfig_cfg.lua
@@ -1,42 +1,43 @@
 local success, lspconfig = pcall(require, "lspconfig")
-local _, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+local cmp_lsp_success, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_lsp_success then
+	return
+end
 
 local capabilities = cmp_lsp.default_capabilities()
 
-if _ then
-	if success then
-		lspconfig.lua_ls.setup({
-			filetypes = { "lua" },
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-						checkThirdParty = false,
-					},
-					telemetry = {
-						enable = false,
-					},
+if success then
+	lspconfig.lua_ls.setup({
+		filetypes = { "lua" },
+		settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				telemetry = {
+					enable = false,
 				},
 			},
-			capabilities = capabilities,
-		})
-		lspconfig.clangd.setup({
-			filetypes = { "cpp" },
-			capabilities = capabilities,
-		})
-		lspconfig.bashls.setup({
-			filetypes = { "sh" },
-			capabilities = capabilities,
-		})
-		lspconfig.marksman.setup({
-			filetypes = { "markdown" },
-			capabilities = capabilities,
-		})
-	end
+		},
+		capabilities = capabilities,
+	})
+	lspconfig.clangd.setup({
+		filetypes = { "cpp" },
+		capabilities = capabilities,
+	})
+	lspconfig.bashls.setup({
+		filetypes = { "sh" },
+		capabilities = capabilities,
+	})
+	lspconfig.marksman.setup({
+		filetypes = { "markdown" },
+		capabilities = capabilities,
+	})
 end
