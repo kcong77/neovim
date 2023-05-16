fe50dea3cdaa86a56e38dd9604dbe4890895fba4
Kay N <kcong.nguyen@icloud.com>
2023-05-15 20:29:31 -0500

    check

--- lua/configs.lua
+++ lua/configs.lua
@@ -1,146 +1,155 @@
-local loader = require("loader_plugins")
+LOADER = require("loader_plugins")
 
-local gruvbox = loader.gruvbox
-local treesitter = loader.treesitter
-local status = loader.status
-local autopairs = loader.autopairs
-local gs = loader.gs
-local impatient = loader.impatient
-local cmp = loader.cmp
-local lsp = loader.lsp
-local capabilities = loader.capabilities
-local mason = loader.mason
-local telescope = loader.telescope
-local mason_config = loader.mason_config
-local mason_nullls = loader.mason_nullls
-local nullls = loader.nullls
+function SETUP()
+	local gruvbox = LOADER.gruvbox
+	local treesitter = LOADER.treesitter
+	local lualine = LOADER.lualine
+	local autopairs = LOADER.autopairs
+	local gs = LOADER.gs
+	local impatient = LOADER.impatient
+	local cmp = LOADER.cmp
+	local lsp = LOADER.lsp
+	local capabilities = LOADER.capabilities
+	local mason = LOADER.mason
+	local telescope = LOADER.telescope
+	local mason_config = LOADER.mason_config
+	local mason_nullls = LOADER.mason_nullls
+	local nullls = LOADER.nullls
 
-gruvbox.setup({
-	underline = false,
-	transparent_mode = true,
-})
-vim.cmd([[colorscheme gruvbox]])
+	gruvbox.setup({
+		underline = false,
+		transparent_mode = true,
+	})
+	vim.cmd([[colorscheme gruvbox]])
 
-treesitter.setup({
-	ensure_installed = { "lua", "c", "cpp", "bash", "markdown_inline" },
-	indent = {
-		enable = true,
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = true,
-	},
-})
+	treesitter.setup({
+		ensure_installed = { "lua", "c", "cpp", "bash", "markdown_inline" },
+		indent = {
+			enable = true,
+		},
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+	})
 
-lsp.lua_ls.setup({
-	filetypes = { "lua" },
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
+	lsp.lua_ls.setup({
+		filetypes = { "lua" },
+		settings = {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+				},
+				diagnostics = {
+					globals = { "vim" },
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				telemetry = {
+					enable = false,
+				},
 			},
 		},
-	},
-	capabilities = capabilities,
-})
-lsp.clangd.setup({
-	filetypes = { "c", "cpp" },
-	capabilities = capabilities,
-})
-lsp.bashls.setup({
-	filetypes = { "sh" },
-	capabilities = capabilities,
-})
-lsp.marksman.setup({
-	filetypes = { "markdown" },
-	capabilities = capabilities,
-})
+		capabilities = capabilities,
+	})
+	lsp.clangd.setup({
+		filetypes = { "c", "cpp" },
+		capabilities = capabilities,
+	})
+	lsp.bashls.setup({
+		filetypes = { "sh" },
+		capabilities = capabilities,
+	})
+	lsp.marksman.setup({
+		filetypes = { "markdown" },
+		capabilities = capabilities,
+	})
 
-mason.setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
+	mason.setup({
+		ui = {
+			icons = {
+				package_installed = "✓",
+				package_pending = "➜",
+				package_uninstalled = "✗",
+			},
 		},
-	},
-})
+	})
 
-mason_nullls.setup({
-	ensure_installed = { "stylua", "clang_format", "shfmt", "prettierd" },
-})
+	mason_nullls.setup({
+		ensure_installed = { "stylua", "clang_format", "shfmt", "prettierd" },
+	})
 
-mason_config.setup({
-	ensure_installed = { "lua_ls", "clangd", "bashls", "marksman" },
-	automatic_installation = false,
-})
+	mason_config.setup({
+		ensure_installed = { "lua_ls", "clangd", "bashls", "marksman" },
+		automatic_installation = false,
+	})
 
-nullls.setup({
-	autostart = true,
-	sources = {
-		nullls.builtins.formatting.stylua.with({
-			filetypes = { "lua" },
-		}),
-		nullls.builtins.formatting.clang_format.with({
-			filetypes = { "c", "cpp" },
-		}),
-		nullls.builtins.formatting.shfmt.with({
-			filetypes = { "sh" },
-		}),
-		nullls.builtins.formatting.prettierd.with({
-			filetypes = { "markdown", "json", "yaml" },
-		}),
-	},
-})
-
-cmp.setup({
-	sources = loader.cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "buffer" },
-		{ name = "path" },
-	}),
-})
-
-telescope.setup({
-	defaults = {
-		layout_strategy = "vertical",
-		layout_config = {
-			width = 0.85,
-			height = 0.85,
+	nullls.setup({
+		autostart = true,
+		sources = {
+			nullls.builtins.formatting.stylua.with({
+				filetypes = { "lua" },
+			}),
+			nullls.builtins.formatting.clang_format.with({
+				filetypes = { "c", "cpp" },
+			}),
+			nullls.builtins.formatting.shfmt.with({
+				filetypes = { "sh" },
+			}),
+			nullls.builtins.formatting.prettierd.with({
+				filetypes = { "markdown", "json", "yaml" },
+			}),
 		},
-	},
-})
+	})
 
-status.setup({
-	options = {
-		icons_enabled = true,
-		theme = "gruvbox",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = { { "filename", path = 1, symbols = { modified = "[~]" } } },
-		lualine_x = { "encoding", "filetype" },
-		lualine_y = { "progress" },
-	},
-})
+	cmp.setup({
+		sources = LOADER.cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "buffer" },
+			{ name = "path" },
+		}),
+	})
 
-autopairs.setup({
-	disable_filetype = { "TelescopePrompt", "vim" },
-})
+	telescope.setup({
+		defaults = {
+			layout_strategy = "vertical",
+			layout_config = {
+				width = 0.85,
+				height = 0.85,
+			},
+		},
+	})
 
-gs.setup()
+	lualine.setup({
+		options = {
+			icons_enabled = true,
+			theme = "gruvbox",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = { { "filename", path = 1, symbols = { modified = "[~]" } } },
+			lualine_x = { "encoding", "filetype" },
+			lualine_y = { "progress" },
+		},
+	})
 
-impatient.enable_profile()
+	autopairs.setup({
+		disable_filetype = { "TelescopePrompt", "vim" },
+	})
+
+	gs.setup()
+
+	impatient.enable_profile()
+end
+
+if LOADER.ALL_PLUGINS_LOADED_SUCCESSFULL == true then
+  SETUP()
+
+else 
+  print("fail")
+end
--- lua/loader_plugins.lua
+++ lua/loader_plugins.lua
@@ -1,93 +1,101 @@
+ALL_PLUGINS_LOADED_SUCCESSFULL = true
+
+function PLUGIN_FAILED_LOAD()
+	return false
+end
+
 local gruvbox_ok, gruvbox = pcall(require, "gruvbox")
 if not gruvbox_ok then
-	print("gruvbox not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local cmp_ok, cmp = pcall(require, "cmp")
 if not cmp_ok then
-	print("cmp not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local treesitter_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not treesitter_ok then
-	print("treesitter not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
-local status_ok, status = pcall(require, "lualine")
-if not status_ok then
-	print("lualine not working!")
+local lualine_ok, lualine = pcall(require, "lualine")
+if not lualine_ok then
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local autopairs_ok, autopairs = pcall(require, "nvim-autopairs")
 if not autopairs_ok then
-	print("autopairs not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local gs_ok, gs = pcall(require, "gitsigns")
 if not gs_ok then
-	print("gitsigns not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local impatient_ok, impatient = pcall(require, "impatient")
 if not impatient_ok then
-	print("impatient not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local lsp_ok, lsp = pcall(require, "lspconfig")
 if not lsp_ok then
-	print("lsp not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local cmp_lsp_ok, cmp_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_lsp_ok then
-	print("cmp_nvim_lsp not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local mason_ok, mason = pcall(require, "mason")
 if not mason_ok then
-	print("mason not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local mason_config_ok, mason_config = pcall(require, "mason-lspconfig")
 if not mason_config_ok then
-	print("mason config not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local mason_nullls_ok, mason_nullls = pcall(require, "mason-null-ls")
 if not mason_nullls_ok then
-	print("mason null_ls not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local nullls_ok, nullls = pcall(require, "null-ls")
 if not nullls_ok then
-	print("null_ls not working")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local telescope_ok, telescope = pcall(require, "telescope")
 if not telescope_ok then
-	print("telescope not working!")
+	ALL_PLUGINS_LOADED_SUCCESSFULL = PLUGIN_FAILED_LOAD()
 	return
 end
 
 local capabilities = cmp_lsp.default_capabilities()
 
 local loader = {
+
+ALL_PLUGINS_LOADED_SUCCESSFULL = ALL_PLUGINS_LOADED_SUCCESSFULL, 
 	gruvbox = gruvbox,
 	treesitter = treesitter,
-	status = status,
+	lualine = lualine,
 	autopairs = autopairs,
 	gs = gs,
 	impatient = impatient,
