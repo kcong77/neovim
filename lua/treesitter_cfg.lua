e87af6a21c2150654acd19adb39fe0345ca01b09
Kay N <kcong.nguyen@icloud.com>
2023-05-16 22:57:46 -0500

    final

--- lua/treesitter_cfg.lua
+++ lua/treesitter_cfg.lua
@@ -2,7 +2,6 @@ local success, treesitter = pcall(require, "nvim-treesitter.configs")
 if success then
 	treesitter.setup({
 		ensure_installed = { "lua", "cpp", "bash", "markdown" },
-		auto_install = true,
 		indent = {
 			enable = true,
 		},
